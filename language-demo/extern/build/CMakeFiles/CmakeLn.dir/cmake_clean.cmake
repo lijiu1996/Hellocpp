81d64d478d6b7af6bac175a4737485c810f6b62b
lijiu1996 <642216155@qq.com>
2019-03-20 01:49:59 +0800

    Git learn

---
+++ language-demo/extern/build/CMakeFiles/CmakeLn.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CmakeLn.dir/main.cpp.o"
+  "CMakeFiles/CmakeLn.dir/test.cpp.o"
+  "CmakeLn.pdb"
+  "CmakeLn"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CmakeLn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
